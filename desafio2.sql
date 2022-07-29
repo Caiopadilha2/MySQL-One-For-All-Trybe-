327e85d1714f46ed8653bd35cc4030ffc7497b7c
Caio <caiopadilha@id.uff.br>
2022-07-29 14:30:22 -0300

    feat: requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,7 @@ SELECT COUNT(c.nome_cancao) AS cancoes,
        COUNT(DISTINCT a.nome_artista) AS artistas,
        COUNT(DISTINCT al.nome_album) AS albuns
 FROM 
-    cancoes c
-    JOIN artistas a
-    JOIN album al
+    cancoes AS c
+    JOIN artistas AS a
+    JOIN album AS al
         ON c.album_id = al.album_id AND al.artista_id = a.artista_id;
